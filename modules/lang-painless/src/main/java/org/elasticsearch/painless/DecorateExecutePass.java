3b01bdda2eaaef52d4149fa99e11956432dd098a
Jack Conradson <osjdconrad@gmail.com>
2019-12-27 16:20:18 -0800

    change scoping to track variables in all scopes

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/DecorateExecutePass.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/DecorateExecutePass.java
@@ -19,16 +19,85 @@
 
 package org.elasticsearch.painless;
 
+import org.elasticsearch.painless.ir.BlockNode;
 import org.elasticsearch.painless.ir.ClassNode;
+import org.elasticsearch.painless.ir.DeclarationNode;
+import org.elasticsearch.painless.ir.FunctionNode;
+import org.elasticsearch.painless.ir.StatementNode;
+import org.elasticsearch.painless.ir.TypeNode;
+import org.elasticsearch.painless.ir.UnboundCallNode;
+import org.elasticsearch.painless.symbol.FunctionTable;
 import org.elasticsearch.painless.symbol.ScriptRoot;
+import org.objectweb.asm.commons.Method;
+
+import java.util.Collections;
 
 public class DecorateExecutePass {
 
     public static void pass(ScriptRoot scriptRoot, ClassNode classNode) {
-        
+        FunctionNode executeFunctionNode = null;
+
+        for (FunctionNode functionNode : classNode.getFunctionsNodes()) {
+            if ("execute".equals(functionNode.getName())) {
+                executeFunctionNode = functionNode;
+                break;
+            }
+        }
+
+        BlockNode blockNode = executeFunctionNode.getBlockNode();
+
+        int statementIndex = 0;
+
+        while (statementIndex < blockNode.getStatementsNodes().size()) {
+            StatementNode statementNode = blockNode.getStatementNode(statementIndex);
+
+            if (statementNode instanceof DeclarationNode) {
+                DeclarationNode declarationNode = (DeclarationNode) statementNode;
+                boolean isRemoved = false;
+
+                for (int getIndex = 0; getIndex < scriptRoot.getScriptClassInfo().getGetMethods().size(); ++getIndex) {
+                    Class<?> returnType = scriptRoot.getScriptClassInfo().getGetReturns().get(getIndex);
+                    Method getMethod = scriptRoot.getScriptClassInfo().getGetMethods().get(getIndex);
+                    String name = getMethod.getName().substring(3);
+                    name = Character.toLowerCase(name.charAt(0)) + name.substring(1);
+
+                    if (name.equals(declarationNode.getName())) {
+                        if (scriptRoot.getUsedVariables().contains(name)) {
+                            declarationNode.setExpressionNode(new UnboundCallNode()
+                                    .setTypeNode(new TypeNode()
+                                            .setLocation(declarationNode.getLocation())
+                                            .setType(declarationNode.getDeclarationType())
+                                    )
+                                    .setLocation(declarationNode.getLocation())
+                                    .setLocalFunction(new FunctionTable.LocalFunction(
+                                            getMethod.getName(), returnType, Collections.emptyList(), true, false
+                                    ))
+                            );
+                        } else {
+                            blockNode.removeStatementNode(statementIndex);
+                            isRemoved = true;
+                        }
+
+                        break;
+                    }
+                }
+
+                if (isRemoved == false) {
+                    ++statementIndex;
+                }
+            } else {
+                ++statementIndex;
+            }
+        }
+
+        /*blockNode = new BlockNode()
+                .addStatementNode(new TryNode())
+                .setLocation(blockNode.getLocation())
+                .setAllEscape(blockNode.doAllEscape())
+                .setStatementCount(blockNode.getStatementCount());*/
     }
 
     protected DecorateExecutePass() {
-
+        // do nothing
     }
 }
