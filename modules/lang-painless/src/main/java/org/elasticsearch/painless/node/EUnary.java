9c845b88a38646a2904a1cbc2de5f7c9f28fcba6
Jack Conradson <osjdconrad@gmail.com>
2020-01-12 10:39:32 -0800

    partially removed constant

--- modules/lang-painless/src/main/java/org/elasticsearch/painless/node/EUnary.java
+++ modules/lang-painless/src/main/java/org/elasticsearch/painless/node/EUnary.java
@@ -73,10 +73,6 @@ public final class EUnary extends AExpression {
         child.analyze(scriptRoot, variables);
         child = child.cast(scriptRoot, variables);
 
-        if (child.constant != null) {
-            constant = !(boolean)child.constant;
-        }
-
         actual = boolean.class;
     }
 
@@ -92,16 +88,6 @@ public final class EUnary extends AExpression {
 
         child.expected = promote;
         child = child.cast(scriptRoot, variables);
-
-        if (child.constant != null) {
-            if (promote == int.class) {
-                constant = ~(int)child.constant;
-            } else if (promote == long.class) {
-                constant = ~(long)child.constant;
-            } else {
-                throw createError(new IllegalStateException("Illegal tree structure."));
-            }
-        }
 
         if (promote == def.class && expected != null) {
             actual = expected;
@@ -123,20 +109,6 @@ public final class EUnary extends AExpression {
         child.expected = promote;
         child = child.cast(scriptRoot, variables);
 
-        if (child.constant != null) {
-            if (promote == int.class) {
-                constant = +(int)child.constant;
-            } else if (promote == long.class) {
-                constant = +(long)child.constant;
-            } else if (promote == float.class) {
-                constant = +(float)child.constant;
-            } else if (promote == double.class) {
-                constant = +(double)child.constant;
-            } else {
-                throw createError(new IllegalStateException("Illegal tree structure."));
-            }
-        }
-
         if (promote == def.class && expected != null) {
             actual = expected;
         } else {
@@ -156,20 +128,6 @@ public final class EUnary extends AExpression {
 
         child.expected = promote;
         child = child.cast(scriptRoot, variables);
-
-        if (child.constant != null) {
-            if (promote == int.class) {
-                constant = -(int)child.constant;
-            } else if (promote == long.class) {
-                constant = -(long)child.constant;
-            } else if (promote == float.class) {
-                constant = -(float)child.constant;
-            } else if (promote == double.class) {
-                constant = -(double)child.constant;
-            } else {
-                throw createError(new IllegalStateException("Illegal tree structure."));
-            }
-        }
 
         if (promote == def.class && expected != null) {
             actual = expected;
