82960188d415c46ed546eae8351f7ce49c4d906b
Jack Conradson <osjdconrad@gmail.com>
2026-03-31 11:15:32 -0700

    Revert "Fix SocketException construction to use initCause for Java 11 compatibility"
    This reverts commit 92f64fac813e2850d7db692814350ac30ed2dbe8.

--- libs/entitlement/src/main/java/org/elasticsearch/entitlement/config/NetworkInstrumentation.java
+++ libs/entitlement/src/main/java/org/elasticsearch/entitlement/config/NetworkInstrumentation.java
@@ -278,42 +278,30 @@ public class NetworkInstrumentation implements InstrumentationConfig {
         });
 
         builder.on(DatagramSocket.class, rule -> {
-            rule.callingStatic(DatagramSocket::new).enforce(Policies::allNetworkAccess).elseThrow(e -> {
-                var ex = new SocketException(e.getMessage());
-                ex.initCause(e);
-                return ex;
-            });
-            rule.callingStatic(DatagramSocket::new, Integer.class).enforce(Policies::allNetworkAccess).elseThrow(e -> {
-                var ex = new SocketException(e.getMessage());
-                ex.initCause(e);
-                return ex;
-            });
-            rule.callingStatic(DatagramSocket::new, Integer.class, InetAddress.class).enforce(Policies::allNetworkAccess).elseThrow(e -> {
-                var ex = new SocketException(e.getMessage());
-                ex.initCause(e);
-                return ex;
-            });
-            rule.callingStatic(DatagramSocket::new, SocketAddress.class).enforce(Policies::allNetworkAccess).elseThrow(e -> {
-                var ex = new SocketException(e.getMessage());
-                ex.initCause(e);
-                return ex;
-            });
+            rule.callingStatic(DatagramSocket::new)
+                .enforce(Policies::allNetworkAccess)
+                .elseThrow(e -> new SocketException(e.getMessage(), e));
+            rule.callingStatic(DatagramSocket::new, Integer.class)
+                .enforce(Policies::allNetworkAccess)
+                .elseThrow(e -> new SocketException(e.getMessage(), e));
+            rule.callingStatic(DatagramSocket::new, Integer.class, InetAddress.class)
+                .enforce(Policies::allNetworkAccess)
+                .elseThrow(e -> new SocketException(e.getMessage(), e));
+            rule.callingStatic(DatagramSocket::new, SocketAddress.class)
+                .enforce(Policies::allNetworkAccess)
+                .elseThrow(e -> new SocketException(e.getMessage(), e));
             rule.callingVoidStatic(DatagramSocket::setDatagramSocketImplFactory, DatagramSocketImplFactory.class)
                 .enforce(Policies::changeNetworkHandling)
                 .elseThrow(IOException::new);
-            rule.callingVoid(DatagramSocket::bind, SocketAddress.class).enforce(Policies::inboundNetworkAccess).elseThrow(e -> {
-                var ex = new SocketException(e.getMessage());
-                ex.initCause(e);
-                return ex;
-            });
+            rule.callingVoid(DatagramSocket::bind, SocketAddress.class)
+                .enforce(Policies::inboundNetworkAccess)
+                .elseThrow(e -> new SocketException(e.getMessage(), e));
             rule.callingVoid(DatagramSocket::connect, InetAddress.class, Integer.class)
                 .enforce(Policies::allNetworkAccess)
                 .elseThrowNotEntitled();
-            rule.callingVoid(DatagramSocket::connect, SocketAddress.class).enforce(Policies::allNetworkAccess).elseThrow(e -> {
-                var ex = new SocketException(e.getMessage());
-                ex.initCause(e);
-                return ex;
-            });
+            rule.callingVoid(DatagramSocket::connect, SocketAddress.class)
+                .enforce(Policies::allNetworkAccess)
+                .elseThrow(e -> new SocketException(e.getMessage(), e));
             rule.callingVoid(DatagramSocket::receive, DatagramPacket.class)
                 .enforce(Policies::inboundNetworkAccess)
                 .elseThrow(IOException::new);
